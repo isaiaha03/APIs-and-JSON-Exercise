4d18857de6a40660a0f461fd98ba9f34147114d3
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-07-29 13:55:01 -0500

    Completed first exercise.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            var client = new HttpClient();
+            var quote = new RonVSKanyeAPI(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("====================================================");
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+                Console.WriteLine();
+                Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+            }
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        private HttpClient _client;
+
+        public RonVSKanyeAPI(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
     }
 }
